ab5135032d6f07993c10999bdc518f78ae00a470
Michele Comotti <como9307@gmail.com>
2026-05-21 23:50:07 +0200

    created ingest class and added as dependency injestion config

--- simple-rag-company-policies/src/ingest.py
+++ simple-rag-company-policies/src/ingest.py
@@ -1,60 +1,51 @@
 from langchain_community.document_loaders import TextLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_chroma import Chroma
-#from langchain_openai import OpenAIEmbeddings
 from langchain_ollama import OllamaEmbeddings
-from dotenv import load_dotenv
-from pathlib import Path
-import os
+from config import Config
 
-DB_PATH = Path("../chroma_db")
-TEXT = "../data/company-policies.txt"
-#EMBEDDING_MODEL = "text-embedding-3-small"
-#EMBEDDING_PROVIDER = "openai_key"
-EMBEDDING_MODEL = "nomic-embed-text"
-EMBEDDING_BASE_URL = "http://localhost:11434"
-load_dotenv()
 
-def load_document():
-    loaders = [
-        TextLoader(TEXT)
-    ]
-    
-    original_docs = []
-    for loader in loaders:
-        original_docs.extend(loader.load())
-    
-    text_splitter = RecursiveCharacterTextSplitter(
-        chunk_size=500,
-        chunk_overlap=50,
-        separators=[
-            "\n================================================================================\n",
-            "\n--------------------------------------------------------------------------------\n",
-            "\n\n",
-            "\n",
-            " ",
-            ""
+class Ingest:
+    def __init__(self,config:Config):
+        self.config = config
+        
+    def load_document(self):
+        loaders = [
+            TextLoader(self.config.document)
         ]
-    )
-    return text_splitter.split_documents(original_docs)
-    
-def create_embedding():
-    return OllamaEmbeddings(
-        model=EMBEDDING_MODEL,
-        base_url=EMBEDDING_BASE_URL
-    )
-    # return OpenAIEmbeddings(
-    #     model=EMBEDDING_MODEL,
-    #     openai_api_key=os.getenv(EMBEDDING_PROVIDER)
-    # )
+        
+        original_docs = []
+        for loader in loaders:
+            original_docs.extend(loader.load())
+        
+        text_splitter = RecursiveCharacterTextSplitter(
+            chunk_size=self.config.chunk_size,
+            chunk_overlap=self.config.chunk_overlap,
+            separators=[
+                "\n================================================================================\n",
+                "\n--------------------------------------------------------------------------------\n",
+                "\n\n",
+                "\n",
+                " ",
+                ""
+            ]
+        )
+        return text_splitter.split_documents(original_docs)
+        
+    def create_embedding(self):
+        return OllamaEmbeddings(
+            model=self.config.embedding_model,
+            base_url=self.config.ollama_base_url
+        )
 
-def save_on_chroma(chunks,embedding):
-    vectorstore = Chroma.from_documents(
-        documents=chunks,
-        embedding=embedding,
-        persist_directory=str(DB_PATH),
-        collection_name="company_policies"
-    )
-    
-    return vectorstore
-    
+
+    def save_on_chroma(self,chunks,embedding):
+        vectorstore = Chroma.from_documents(
+            documents=chunks,
+            embedding=embedding,
+            persist_directory=str(self.config.db_path),
+            collection_name="company_policies"
+        )
+        
+        return vectorstore
+        
