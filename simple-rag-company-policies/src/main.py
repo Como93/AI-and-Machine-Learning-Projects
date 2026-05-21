0526d12d26e9e308da2b5c52e6bd49eb5ca70fc1
Michele Comotti <como9307@gmail.com>
2026-05-21 23:50:39 +0200

    added config and logging

--- simple-rag-company-policies/src/main.py
+++ simple-rag-company-policies/src/main.py
@@ -1,37 +1,41 @@
-from ingest import load_document,create_embedding,save_on_chroma
-from retrieve import retrieve_top_three
-from generate import generate_answer_with_sources
+from ingest import Ingest
+from retrieve import Retrieve
+from generate import Generate
 import argparse
-from pathlib import Path
+from config import Config
+import logging
 
 
-def setup_database():
-    chunks = load_document()
-    embeddings = create_embedding()
-    vectorstore = save_on_chroma(chunks, embeddings)
+def setup_database(config):
+    ingest = Ingest(config)
+    chunks = ingest.load_document()
+    embeddings = ingest.create_embedding()
+    vectorstore = ingest.save_on_chroma(chunks, embeddings)
     
-    print("Database setup complete!")
+    logging.info(f"Setup Database Completed")
     
     return vectorstore
 
-def ask_question(question):
-    print("Retrieving relevant documents")
-    documents = retrieve_top_three(question)
+def ask_question(question,config):
+    logging.info(f"Retrieving relevant documents")
+    retrieve = Retrieve(config)
+    documents = retrieve.retrieve_top_three(question)
     
     if not documents:
-        print("No documents found")
+        logging.info(f"No documents found")
         return
 
-    answer, sources = generate_answer_with_sources(question, documents)
+    generate = Generate(config)
+    answer, sources = generate.generate_answer_with_sources(question, documents)
     
-    print("ANSWER:")
-    print(answer)
+    logging.info(f"ANSWER:")
+    logging.info(f"{answer}")
     
-    print("SOURCES")
+    logging.info(f"SOURCES")
     for source in sources:
-        print(f"\n[Source {source['id']}]")
-        print(f"File: {source['source']}")
-        print(f"Preview: {source['content_preview']}")
+        logging.info(f"\n[Source {source['id']}]")
+        logging.info(f"File: {source['source']}")
+        logging.info(f"Preview: {source['content_preview']}")
     
 
 def main():
@@ -39,13 +43,14 @@ def main():
     parser.add_argument("--setup",action="store_true",help="Setup database")
     parser.add_argument("--question", type=str, help="Single question to answer")
     args = parser.parse_args()
-    
+    config = Config()
+    logging.basicConfig(filename='CompanyPolicy.log', level=logging.INFO)
     
     if args.setup:
-        setup_database()
+        setup_database(config)
         return
     elif args.question:
-        ask_question(args.question)
+        ask_question(args.question,config)
     
 
     
