ad53597c2f9e3b3879e5a4db7e2f0fe0c7a56d4d
Michele Comotti <como9307@gmail.com>
2026-05-24 21:47:49 +0200

    deleted logging libraries

--- simple-rag-company-policies/src/main.py
+++ simple-rag-company-policies/src/main.py
@@ -3,39 +3,38 @@ from retrieve import Retrieve
 from generate import Generate
 import argparse
 from config import Config
-import logging
-
 
 def setup_database(config):
     ingest = Ingest(config)
     chunks = ingest.load_document()
     embeddings = ingest.create_embedding()
     vectorstore = ingest.save_on_chroma(chunks, embeddings)
+    Retrieve.clear_vectorestore()
     
-    logging.info(f"Setup Database Completed")
+    print(f"Setup Database Completed")
     
     return vectorstore
 
 def ask_question(question,config):
-    logging.info(f"Retrieving relevant documents")
+    print(f"Retrieving relevant documents")
     retrieve = Retrieve(config)
     documents = retrieve.retrieve_top_three(question)
     
     if not documents:
-        logging.info(f"No documents found")
+        print(f"No documents found")
         return
 
     generate = Generate(config)
     answer, sources = generate.generate_answer_with_sources(question, documents)
     
-    logging.info(f"ANSWER:")
-    logging.info(f"{answer}")
+    print(f"ANSWER:")
+    print(f"{answer}")
     
-    logging.info(f"SOURCES")
+    print(f"SOURCES")
     for source in sources:
-        logging.info(f"\n[Source {source['id']}]")
-        logging.info(f"File: {source['source']}")
-        logging.info(f"Preview: {source['content_preview']}")
+        print(f"\n[Source {source['id']}]")
+        print(f"File: {source['source']}")
+        print(f"Preview: {source['content_preview']}")
     
 
 def main():
@@ -44,7 +43,6 @@ def main():
     parser.add_argument("--question", type=str, help="Single question to answer")
     args = parser.parse_args()
     config = Config()
-    logging.basicConfig(filename='CompanyPolicy.log', level=logging.INFO)
     
     if args.setup:
         setup_database(config)
