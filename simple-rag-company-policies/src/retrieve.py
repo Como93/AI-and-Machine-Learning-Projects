68d4b9577484c7fd9036b2d666ff4f2306335e82
Michele Comotti <como9307@gmail.com>
2026-05-25 00:10:09 +0200

    added max_marginal_relevance_search for relevance and diversity chunk

--- simple-rag-company-policies/src/retrieve.py
+++ simple-rag-company-policies/src/retrieve.py
@@ -31,7 +31,7 @@ class Retrieve:
 
         return vectorstore
 
-    def retrieve_top_three(self,query: str):
+    def retrieve_top_three(self,query: str,use_mmr: bool = False):
         if not query:
             print(f"Empty query")
             return []
@@ -42,7 +42,11 @@ class Retrieve:
         
         try:
             vectorstore = self.load_vectorstore()
-            results = vectorstore.similarity_search(query, k=self.config.top_k)
+            results = None
+            if use_mmr:
+                results = vectorstore.max_marginal_relevance_search(query, k=self.config.top_k,lambda_mult=0.5)
+            else:
+                results = vectorstore.similarity_search(query, k=self.config.top_k)
             return results
         except Exception as e:
             print(f"Retrieval error {e}")
